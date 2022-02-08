44175959a5b4d1efae76291f3b2baf0fde2eba6d
Alex Tolkachov <kadokan81@gmail.com>
2022-02-09 00:01:38 +0200

    connect react-bootstrap with react router link in navigation bar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 	return (
 		<header>
 			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
 				<Container>
-					<Navbar.Brand href='#home'>ProShop</Navbar.Brand>
+					<Link to='/' className='navbar-brand'>
+						ProShop
+					</Link>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
-					{/* <Navbar.Collapse id='basic-navbar-nav'> */}
-					<Nav className='ml-auto'>
-						<Nav.Link href='/cart'>
-							<i className='fas fa-shopping-cart'></i>Cart
-						</Nav.Link>
-						<Nav.Link href='/login'>
-							<i className='fas fa-user'></i>Sign In
-						</Nav.Link>
-					</Nav>
-					{/* </Navbar.Collapse> */}
+					<Navbar.Collapse id='basic-navbar-nav'>
+						<Nav className='ml-auto' style={{ marginLeft: 'auto' }}>
+							<Link className='nav-link' to='cart'>
+								<i className='fas fa-shopping-cart'></i>Card
+							</Link>
+
+							<Link className='nav-link' to='/login'>
+								<i className='fas fa-user'></i>Sign In
+							</Link>
+						</Nav>
+					</Navbar.Collapse>
 				</Container>
 			</Navbar>
 		</header>
