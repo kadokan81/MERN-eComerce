f294ddc5e0c4929aed8d00d65b6c6c739fa9ba6b
Alex Tolkachov <kadokan81@gmail.com>
2022-02-09 13:25:14 +0200

    add ProductScreen page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,71 @@
 import React from 'react';
+import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
+import Raiting from '../components/Rating';
+import products from '../products';
 
 const ProductScreen = () => {
-	return <h2>Product screen</h2>;
+	let params = useParams();
+	const product = products.find((p) => p._id === params.id);
+
+	return (
+		<>
+			<Link to='/' className='btn btn-light my3'>
+				Go Back
+			</Link>
+			<Row>
+				<Col md={6}>
+					<Image src={product.image} alt={product.name} fluid />
+				</Col>
+				<Col md={3}>
+					<ListGroup variant='flush'>
+						<ListGroup.Item>
+							<h3>{product.name}</h3>
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<Raiting
+								value={product.rating}
+								text={`${product.numReviews} reviews`}
+							/>
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<h3>Price: ${product.price}</h3>
+						</ListGroup.Item>
+						<ListGroup.Item>Description: {product.description}</ListGroup.Item>
+					</ListGroup>
+				</Col>
+				<Col md={3}>
+					<Card>
+						<ListGroup variant='flush'>
+							<ListGroup.Item>
+								<Row>
+									<Col>Status:</Col>
+									<Col>
+										{product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+									</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Price:</Col>
+									<Col>{product.price}</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<div className='d-grid gap-2'>
+									<Button
+										variant='primary'
+										disabled={product.countInStock === 0}>
+										Add To Card
+									</Button>
+								</div>
+							</ListGroup.Item>
+						</ListGroup>
+					</Card>
+				</Col>
+			</Row>
+		</>
+	);
 };
 
 export default ProductScreen;
